e837f86eea7905a6203dc9e8e7a62ffe9ab54e30
venkateshsanjana05 <venkateshsanjana705@gmail.com>
2026-09-24 02:00:28 +0530

    Updated Resume Sep 2026

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Link, NavLink, useNavigate, use
 
 import { useState, useRef, useEffect, useMemo } from 'react';
 import songFile from './assets/LEASE by Takeshi Abo but slightly bitcrushed for nostalgia - (64 Kbps).mp3';
-import resumeFile from './assets/SanjanaVenkatesh_SDE.pdf.pdf';
+import resumeFile from './assets/Sanjana_V_SDE.pdf.pdf';
 import profilePhoto from './assets/digicamfx-original-2026-08-15T10-55-28.jpg';
 import './App.css';
 
